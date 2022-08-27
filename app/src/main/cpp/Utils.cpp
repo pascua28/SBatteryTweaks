446421106d8b1ae1a117a259b3ea3437a56a750c
Samuel Pascua <sgpascua@ngcp.ph>
2022-08-27 10:33:21 +0800

    Utils: use fopen() instead of ifstream()
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- app/src/main/cpp/Utils.cpp
+++ app/src/main/cpp/Utils.cpp
@@ -5,20 +5,40 @@
 #include <iostream>
 #include <fstream>
 #include <jni.h>
+#include <sstream>
+#include <strstream>
+#include <regex>
+#include <android/log.h>
 
 using std::string;
 using std::ifstream;
 
+string convertToString(char* a, int size)
+{
+    int i;
+    string s = "";
+    for (i = 0; i < size; i++) {
+        s = s + a[i];
+    }
+    return s;
+}
+
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_sammy_chargerateautomator_Utils_readFile(JNIEnv *env, jobject thiz, jstring file_path) {
-    string ret;
     string filePath = (*env).GetStringUTFChars(file_path, JNI_FALSE);
-    ifstream file(filePath);
-    if (!file.is_open())
+    char line[4];
+
+    FILE *fp = fopen(filePath.c_str(), "r");
+
+    if (fp == NULL)
         return env->NewStringUTF("0");
-    file >> ret;
-    file.close();
+    
+    fgets(line, 4, fp);
+    fclose(fp);
+
+    string ret(line);
+    ret.erase(std::remove(ret.begin(), ret.end(), '\n'), ret.cend());
 
     return env->NewStringUTF(ret.c_str());
 }
