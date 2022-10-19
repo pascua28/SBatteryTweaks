c7c787134792536a510feb0c0b5de7e675799ec5
Samuel Pascua <sgpascua@ngcp.ph>
2022-10-19 10:22:13 +0800

    Revert "Utils: migrate file operations to C++"
    This reverts commit f17d6fbf5e246468bdca1e7f2001b2a041d8f1f9.

--- app/src/main/java/com/sammy/sbatterytweaks/Utils.java
+++ app/src/main/java/com/sammy/sbatterytweaks/Utils.java
@@ -5,15 +5,41 @@ import static java.lang.Boolean.TRUE;
 import android.content.Context;
 
 import com.topjohnwu.superuser.Shell;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 public class Utils {
-    static {
-        System.loadLibrary("Utils");
-    }
+    private static StringBuilder stringBuilder = new StringBuilder();
+    private static BufferedReader buf = null;
 
-    public static native String readFile(String filePath);
+    public static String readFile(String filePath) {
+        File file = new File(filePath);
+        buf = null;
+        try {
+            buf = new BufferedReader(new FileReader(file));
+            stringBuilder.setLength(0);
+
+            String line;
+            while ((line = buf.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+
+            return stringBuilder.toString();
+        } catch (IOException ignored) {
+        } finally {
+            try {
+                if (buf != null) buf.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return "0";
+    }
 
     public static boolean isRooted() {
         Shell.getShell();
