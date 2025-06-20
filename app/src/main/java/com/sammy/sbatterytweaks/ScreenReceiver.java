68d6eabc891a78dcef898c0b9cd03e3dfea4635d
Samuel Pascua <sgpascua@ngcp.ph>
2025-06-20 22:45:48 +0800

    Always update battery drain data when changing screen state
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- app/src/main/java/com/sammy/sbatterytweaks/ScreenReceiver.java
+++ app/src/main/java/com/sammy/sbatterytweaks/ScreenReceiver.java
@@ -11,9 +11,11 @@ public class ScreenReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         switch (Objects.requireNonNull(intent.getAction())) {
             case Intent.ACTION_SCREEN_ON:
+                DrainMonitor.handleBatteryChange(context, BatteryReceiver.ratedCapacity, BatteryReceiver.isCharging());
                 DrainMonitor.handleScreenChange(true);
                 break;
             case Intent.ACTION_SCREEN_OFF:
+                DrainMonitor.handleBatteryChange(context, BatteryReceiver.ratedCapacity, BatteryReceiver.isCharging());
                 DrainMonitor.handleScreenChange(false);
                 break;
         }
